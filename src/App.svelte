6ce1659190fb4790b0481bf919b8224f7c76ca0d
Andrew Wooldridge <triptych@gmail.com>
2020-08-30 10:25:06 -0700

    playing with observable

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <script>
 	import TailwindStyles from './TailwindStyles.svelte';
-	import * as rxjs from 'rxjs';
-	
+
+	import { Observable } from 'rxjs';
+
 	const message = 'Learn Svelte';
-	console.log(rxjs.of(1, 2, 3));
+
+
+
+	const observable = new Observable(subscriber => {
+	subscriber.next(1);
+	subscriber.next(2);
+	subscriber.next(3);
+	setTimeout(() => {
+		subscriber.next(4);
+		subscriber.complete();
+	}, 1000);
+	});
+ 
+	console.log('just before subscribe');
+	observable.subscribe({
+  		next(x) { console.log('got value ' + x); },
+  		error(err) { console.error('something wrong occurred: ' + err); },
+  		complete() { console.log('done'); }
+	});
+	console.log('just after subscribe');
+
 </script>
 
 <style>
